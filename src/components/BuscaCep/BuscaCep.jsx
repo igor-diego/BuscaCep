2f4e7d30b765a4825815a297f0426e65683616b0
Garridopedro <pe.santos.garrido@gmail.com>
2022-08-18 20:31:23 -0300

    Retornando informações da requisição

--- src/components/BuscaCep/BuscaCep.jsx
+++ src/components/BuscaCep/BuscaCep.jsx
@@ -2,24 +2,28 @@ import React from 'react'
 import { useState } from 'react'
 
 export const BuscaCep = () => {
-    const [cep, setCep] = useState()
-    const inputCep = (e)=> {
-      setCep(e.target.value)
+  const [cep, setCep] = useState()
+  const inputCep = (e) => {
+    setCep(e.target.value)
   }
 
-    async function handleCep() {
-        const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-        const json = await resposta.json()
-        console.log(json);
-        setCep()
-        
-    }
+  async function handleCep() {
+    const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    const json = await resposta.json()
+    console.log(json);
+    setCep(json)
+
+  }
+
   return (
     <div>
-    <input type="text" onChange={inputCep} />
-    <button onClick={handleCep}>buscar</button>
-    {/* <form action="">{cep ? cep.logradouro : 'carregando'}</form> */}
-    <h1>{cep ? cep.logradouro : 'carregando'}</h1>
+      <input type="text" onChange={inputCep} />
+      <button onClick={handleCep}>buscar</button>
+      <div>
+        <h2>{cep ? cep.logradouro : ''}</h2>
+        <h2>{cep ? cep.bairro : ''}</h2>
+        <h2>{cep ? cep.localidade : ''}</h2>
+      </div>
     </div>
   )
 }
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export const Section = () => {
+  return (
+    <section>{}</section>
+  )
+}

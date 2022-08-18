fbf0e526833d9a7463136de893b7f3c342992f7c
Garridopedro <pe.santos.garrido@gmail.com>
2022-08-18 19:59:07 -0300

    Adicionando onChange e onClick

--- src/components/BuscaCep/BuscaCep.jsx
+++ src/components/BuscaCep/BuscaCep.jsx
@@ -3,17 +3,23 @@ import { useState } from 'react'
 
 export const BuscaCep = () => {
     const [cep, setCep] = useState()
+    const inputCep = (e)=> {
+      setCep(e.target.value)
+  }
 
     async function handleCep() {
-        const resposta = await fetch('https://viacep.com.br/ws/01001000/json/')
+        const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         const json = await resposta.json()
+        console.log(json);
         setCep()
         
     }
   return (
     <div>
-    <input type="text" />
-      <button>buscar</button>
+    <input type="text" onChange={inputCep} />
+    <button onClick={handleCep}>buscar</button>
+    {/* <form action="">{cep ? cep.logradouro : 'carregando'}</form> */}
+    <h1>{cep ? cep.logradouro : 'carregando'}</h1>
     </div>
   )
 }
